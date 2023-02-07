c1b1792a5b26bd4c4f3c1eed41b97b5044cc9cd0
Njunji Muchai <samwelnjunji@gmail.com>
2023-02-07 17:33:39 +0300

    First Commit

---
+++ src/test/java/com/awsImageUpload/awsImageUpload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsImageUpload.awsImageUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
