f12647dcda80799bd97b82d6bc67333ace33d69f
Njunji Muchai <samwelnjunji@gmail.com>
2023-02-08 21:53:21 +0300

    Added FakeUserProfileDataStore

---
+++ src/main/java/com/awsImageUpload/awsImageUpload/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,18 @@
+package com.awsImageUpload.awsImageUpload.datastore;
+
+import com.awsImageUpload.awsImageUpload.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+    private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+    static{
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), username:"njunjigoat", userProfileImageLink:null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), username:"samwelmbuzi", userProfileImageLink:null));
+    }
+}
